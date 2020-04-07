4ace6127a32b91f65b543c9cccc8f30590fb7cbd
Rahit <rahitkar@users.noreply.github.com>
2020-04-07 17:44:18 +0530

    added display_notes function in main.c to print note counts in a human readeble format

--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include "atm.h"
 
+int main(void);
+void display_notes(unsigned int);
+
+void display_notes(unsigned int notes)
+{
+  int note_amounts[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
+
+  for (int i = 0; i < 8; i++)
+  {
+    printf("%d notes of rs %d\n", notes >> 28, note_amounts[i]);
+    notes = notes << 4;
+  }
+}
+
 int main(void)
 {
   unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999};
-  int note_amounts[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
 
   for (int i = 0; i < 6; i++)
   {
-    unsigned int notes = get_money(amounts[i]);
-    for (int i = 0; i < 8; i++)
-    {
-      printf("%d notes of rs %d\n", notes >> 28, note_amounts[i]);
-      notes = notes << 4;
-    }
+    display_notes(get_money(amounts[i]));
     printf("\n");
   }
   return 0;
