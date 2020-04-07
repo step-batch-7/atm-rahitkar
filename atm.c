4a08d484831f5d817036f17f7ad2bb1e433c1b0d
Rahit <rahitkar@users.noreply.github.com>
2020-04-07 14:43:50 +0530

    changed get_money function to get the note counts

--- atm.c
+++ atm.c
@@ -1,5 +1,15 @@
 #include "atm.h"
 
 unsigned int get_money(unsigned short int amount) {
-  return 1;
+  int indicator = 1, money = amount;
+  int notes[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
+
+  for (int indx = 0; indx < 8; indx++)
+  {
+    indicator = indicator << 4;
+    indicator = indicator + (money / notes[indx]);
+    money = money % notes[indx];
+  }
+  
+  return indicator;
 }
