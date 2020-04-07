297ede95e807a46a78aade4948c0f2f415109523
Rahit <rahitkar@users.noreply.github.com>
2020-04-07 17:27:03 +0530

    changed main function to print note counts in a human readeble format

--- main.c
+++ main.c
@@ -4,11 +4,17 @@
 int main(void)
 {
   unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999};
+  int note_amounts[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
+
   for (int i = 0; i < 6; i++)
   {
     unsigned int notes = get_money(amounts[i]);
-    printf("%5u %08x\n", amounts[i], notes);
+    for (int i = 0; i < 8; i++)
+    {
+      printf("%d notes of rs %d\n", notes >> 28, note_amounts[i]);
+      notes = notes << 4;
+    }
+    printf("\n");
   }
-
   return 0;
 }
